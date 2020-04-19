4fe6000499c03f6f582a0d9b0d02d9e448224a15
Hubertus J J van Dam <hvandam@bnl.gov>
2020-04-19 17:38:35 -0400

    Small fix.

--- ProcessingScripts/Dock/example.sh
+++ ProcessingScripts/Dock/example.sh
@@ -3,7 +3,7 @@
 rm -rf DB-2133 DB-2858 DB-3969 DB-7596 DB-9218
 rm -f  grid.nrg grid.bmp grid.in grid.out
 rm -f  DB-2133_anchor_and_grow_conformers.mol2 DB-2133_anchor_and_grow_scored.mol2
-rm -f  DB-2158_anchor_and_grow_conformers.mol2 DB-2158_anchor_and_grow_scored.mol2
+rm -f  DB-2858_anchor_and_grow_conformers.mol2 DB-2858_anchor_and_grow_scored.mol2
 rm -f  DB-3969_anchor_and_grow_conformers.mol2 DB-3969_anchor_and_grow_scored.mol2
 rm -f  DB-7596_anchor_and_grow_conformers.mol2 DB-7596_anchor_and_grow_scored.mol2
 rm -f  DB-9218_anchor_and_grow_conformers.mol2 DB-9218_anchor_and_grow_scored.mol2
